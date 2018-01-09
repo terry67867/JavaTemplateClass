9753538c6528e349eaadbdf42c1d5059855c2173
terry67867 <terry67867@naver.com>
2018-01-09 08:10:07 +0000

    final

--- TemplateClass/app/assets/javascripts/application.js
+++ TemplateClass/app/assets/javascripts/application.js
@@ -10,7 +10,37 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
+//= require jquery-migrate
 //= require jquery_ujs
+//= require popper
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+//= require hs.megamenu
+//= require ui/widget
+//= require ui/version
+//= require ui/keycode
+//= require ui/position
+//= require ui/unique-id
+//= require ui/safe-active-element
+//= require widgets/menu
+//= require widgets/mouse
+//= require widgets/datepicker
+
+//= require chosen.jquery
+//= require slick
+//= require jquery.fancybox
+
+//= require hs.core
+//= require components/hs.header
+//= require helpers/hs.hamburgers
+//= require components/hs.dropdown
+//= require components/hs.slider
+//= require components/hs.select
+//= require components/hs.carousel
+//= require components/hs.popup
+//= require components/hs.datepicker
+//= require components/hs.go-to
